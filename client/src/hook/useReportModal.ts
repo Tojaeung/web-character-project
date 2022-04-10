d7f3403d6e7880abacdcbd157a6b47e3dd13ea39
Tojaeung <kimwoon11@naver.com>
2022-04-11 01:52:38 +0900

    Feat: "Report모달(신고하기) 오픈시 뒷배경 스크롤 방지 구현"

--- client/src/hook/useReportModal.ts
+++ client/src/hook/useReportModal.ts
@@ -5,10 +5,12 @@ const useReportModal = () => {
 
   const openReportModal = () => {
     setIsOpen(true);
+    document.body.style.overflow = 'hidden'; // 모달 뒤에 화면 고정
   };
 
   const closeReportModal = () => {
     setIsOpen(false);
+    document.body.style.overflow = 'unset'; // 모달 뒤에 화면 고정 해제
   };
 
   return { isOpen, openReportModal, closeReportModal };
