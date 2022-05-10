2d95da75ca776072792c274b9dc3cc4a5f5fcd4b
Tojaeung <kimwoon11@naver.com>
2022-05-10 11:17:55 +0900

    Fixed: "A non-serializable value was detected in the state 에러해결(redux-persist관련)"

--- client/src/store/app/store.ts
+++ client/src/store/app/store.ts
@@ -25,7 +25,8 @@ const persistedReducer = persistReducer(
 
 export const store = configureStore({
   reducer: persistedReducer,
-  devTools: process.env.NODE_ENV !== 'production',
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
+  devTools: process.env.REACT_APP_ENV !== 'production',
 });
 
 export type RootState = ReturnType<typeof store.getState>;
