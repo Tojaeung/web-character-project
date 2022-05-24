826bf624c236a5b1b8b133abe3fd76d953e496a4
Tojaeung <kimwoon11@naver.com>
2022-05-24 17:02:48 +0900

    Fixed: "pm2설정파일 수정"

--- server/pm2.config.js
+++ server/pm2.config.js
@@ -3,15 +3,13 @@ module.exports = {
     {
       name: 'web-character-project',
       script: './dist/server.js',
-      watch: true,
+      watch: false,
       exec_mode: 'cluster',
-      instances: -1,
-      autorestart: true,
+      instances: 'max',
       max_memory_restart: '512M',
       listen_timeout: 3000,
       kill_timeout: 6000,
       combine_logs: true,
-      ignore_watch: ['node_modules'],
     },
   ],
 };
