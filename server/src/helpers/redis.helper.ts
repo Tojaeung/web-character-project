14eaba2a0270aea2d25d3b345e548f0311dbdccc
Tojaeung <kimwoon11@naver.com>
2022-06-01 17:20:30 +0900

    Fixed: "redis설정 오류 수정"

--- server/src/helpers/redis.helper.ts
+++ server/src/helpers/redis.helper.ts
@@ -2,7 +2,7 @@ import Redis from 'ioredis';
 import logger from './winston.helper';
 import 'dotenv/config';
 
-const redis = new Redis({ port: Number(process.env.REDIS_PORT), host: process.env.REDIS_HOST });
+const redis = new Redis(Number(process.env.REDIS_PORT), process.env.REDIS_HOST);
 
 redis?.on('connect', () => {
   logger.info('redis 연결');
