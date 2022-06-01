9ba4d154a17c720add426f4faf36466d80c68d84
Tojaeung <kimwoon11@naver.com>
2022-06-01 17:55:26 +0900

    Fixed: "클라이언트 소켓io 설정값 환경변수로 조정"

--- client/src/utils/socket.ts
+++ client/src/utils/socket.ts
@@ -1,7 +1,8 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5000', {
+const socket = io(process.env.REACT_APP_SOCKET_ENDPOINT as string, {
   autoConnect: false,
+  transports: ['websocket'],
   withCredentials: true,
 });
 
